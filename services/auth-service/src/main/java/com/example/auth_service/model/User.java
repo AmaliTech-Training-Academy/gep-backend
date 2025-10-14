f8a70025a0bccb64f356a5f9c1d076ef2384946f
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-10-14 14:20:43 +0000

    feat: add user entity

---
+++ services/auth-service/src/main/java/com/example/auth_service/model/User.java
@@ -0,0 +1,57 @@
+package com.example.auth_service.model;
+
+import com.example.auth_service.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Column(name = "is_active")
+    boolean isActive;
+
+    private String profileImageUrl;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    private String address;
+
+    @Column(name = "created_at", nullable =false, updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = Instant.now();
+        updatedAt = Instant.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = Instant.now();
+    }
+}
