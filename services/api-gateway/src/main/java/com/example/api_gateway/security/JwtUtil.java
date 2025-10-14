97253d808a28ca68c0d8d68d2aff7ebebaa55e96
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-10-14 23:19:05 +0000

    feat: setup api gateway and register auth service with it

---
+++ services/api-gateway/src/main/java/com/example/api_gateway/security/JwtUtil.java
@@ -0,0 +1,40 @@
+package com.example.api_gateway.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${JWT_SECRET}")
+    private String jwtSecret;
+
+    private SecretKey getKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extractUsername(String token){
+        return extractAllClaims(token).getSubject();
+    }
+
+    protected boolean validateToken(String token){
+        String username = extractUsername(token);
+        return username != null && !extractAllClaims(token).getExpiration().before(new Date());
+    }
+}
