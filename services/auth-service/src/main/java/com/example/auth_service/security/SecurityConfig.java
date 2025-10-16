655996a15ec370dada598170acfcce4dd6719b12
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-10-16 09:06:45 +0000

    feat: allow actuator endpoints

--- services/auth-service/src/main/java/com/example/auth_service/security/SecurityConfig.java
+++ services/auth-service/src/main/java/com/example/auth_service/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 );
 
