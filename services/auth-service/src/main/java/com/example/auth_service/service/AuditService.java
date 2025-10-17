5aec2fc4bb5df0f2768ef116e3f2e9de65c0acc3
Roger Satsi <rogersatsi98@gmail.com>
2025-10-17 14:33:51 +0000

    feat: create audit service interface

--- services/auth-service/src/main/java/com/example/auth_service/service/AuditService.java
+++ services/auth-service/src/main/java/com/example/auth_service/service/AuditService.java
@@ -1,4 +1,12 @@
 package com.example.auth_service.service;
 
+import com.example.auth_service.dto.request.AuditLogRequest;
+import com.example.auth_service.dto.response.PagedAuditResponse;
+import com.example.auth_service.model.AuditLog;
+
+import java.util.List;
+
 public interface AuditService {
+    void save(AuditLogRequest auditLogRequest);
+    PagedAuditResponse findAll(int pageNumber, int pageSize, String[] sortBy);
 }
