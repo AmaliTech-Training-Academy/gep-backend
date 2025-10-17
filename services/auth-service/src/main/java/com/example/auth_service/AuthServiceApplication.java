caf3c4b673181db94a8344467ef28094daef5ecd
Roger Satsi <rogersatsi98@gmail.com>
2025-10-17 14:35:01 +0000

    feat: enable mongo auditing and repository

--- services/auth-service/src/main/java/com/example/auth_service/AuthServiceApplication.java
+++ services/auth-service/src/main/java/com/example/auth_service/AuthServiceApplication.java
@@ -2,12 +2,16 @@ package com.example.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
+@EnableMongoAuditing
 public class AuthServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
 
 }
