bc10b37254ddcb2bcb6c3dd85a88e1601e3ea268
Roger Satsi <rogersatsi98@gmail.com>
2025-10-17 14:33:06 +0000

    feat: create audit mapper interface

--- services/auth-service/src/main/java/com/example/auth_service/mappers/AuditMapper.java
+++ services/auth-service/src/main/java/com/example/auth_service/mappers/AuditMapper.java
@@ -1,4 +1,10 @@
 package com.example.auth_service.mappers;
 
+import com.example.auth_service.dto.response.AuditResponse;
+import com.example.auth_service.model.AuditLog;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface AuditMapper {
+    AuditResponse toAuditResponse(AuditLog auditLog);
 }
