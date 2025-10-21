5c3d8259aba9ae84e0c738af1311bd0ab6b059ef
Roger Satsi <rogersatsi98@gmail.com>
2025-10-21 23:29:17 +0000

    feat: create event meeting type service implementation

--- services/event-service/src/main/java/com/event_service/event_service/services/EventMeetingTypeServiceImpl.java
+++ services/event-service/src/main/java/com/event_service/event_service/services/EventMeetingTypeServiceImpl.java
@@ -1,4 +1,47 @@
 package com.event_service.event_service.services;
 
-public class EventMeetingTypeServiceImpl {
+import com.event_service.event_service.dto.EventMeetingTypeRequest;
+import com.event_service.event_service.dto.EventMeetingTypeResponse;
+import com.event_service.event_service.exceptions.ResourceNotFound;
+import com.event_service.event_service.mappers.EventMeetingMapper;
+import com.event_service.event_service.models.EventMeetingType;
+import com.event_service.event_service.repositories.EventMeetingTypeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class EventMeetingTypeServiceImpl implements EventMeetingTypeService {
+
+    private final EventMeetingTypeRepository eventMeetingTypeRepository;
+    private final  EventMeetingMapper eventMeetingMapper;
+
+    @Override
+    public void createEventMeetingType(EventMeetingTypeRequest eventMeetingTypeRequest) {
+        EventMeetingType eventMeetingType = EventMeetingType.builder().name(eventMeetingTypeRequest.name()).build();
+        eventMeetingTypeRepository.save(eventMeetingType);
+    }
+
+    @Override
+    public EventMeetingType findEventMeetingTypeById(Long id) {
+        return eventMeetingTypeRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFound("EventMeetingType not found"));
+    }
+
+    @Override
+    public List<EventMeetingTypeResponse> findAllEventMeetingTypes() {
+        return eventMeetingTypeRepository.findAll()
+                .stream().map(eventMeetingMapper::toEventMeetingTypeResponse)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void updateEventMeetingType(Long id, EventMeetingTypeRequest eventMeetingTypeRequest) {
+        EventMeetingType eventMeetingType = findEventMeetingTypeById(id);
+        eventMeetingType.setName(eventMeetingTypeRequest.name());
+        eventMeetingTypeRepository.save(eventMeetingType);
+    }
 }
