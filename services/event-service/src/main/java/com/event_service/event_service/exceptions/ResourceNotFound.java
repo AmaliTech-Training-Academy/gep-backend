7667e81f1f21da873b906d6511bd140037d7dd76
Roger Satsi <rogersatsi98@gmail.com>
2025-10-21 22:34:36 +0000

    feat: add resource not found exception signature

--- services/event-service/src/main/java/com/event_service/event_service/exceptions/ResourceNotFound.java
+++ services/event-service/src/main/java/com/event_service/event_service/exceptions/ResourceNotFound.java
@@ -1,7 +1,7 @@
 package com.event_service.event_service.exceptions;
 
 public class ResourceNotFound extends RuntimeException {
-  public ResourceNotFound(String message) {
-    super(message);
-  }
+    public ResourceNotFound(String message) {
+        super(message);
+    }
 }
