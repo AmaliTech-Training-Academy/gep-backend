cc2056218833fad1f0bb2132504440712a9505f6
Roger Satsi <rogersatsi98@gmail.com>
2025-10-21 22:41:29 +0000

    feat: add event type repository

--- services/event-service/src/main/java/com/event_service/event_service/repositories/EventTypeRepository.java
+++ services/event-service/src/main/java/com/event_service/event_service/repositories/EventTypeRepository.java
@@ -1,4 +1,10 @@
 package com.event_service.event_service.repositories;
 
-public interface EventTypeRepository {
+
+import com.event_service.event_service.models.EventType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventTypeRepository extends JpaRepository<EventType, Long> {
 }
