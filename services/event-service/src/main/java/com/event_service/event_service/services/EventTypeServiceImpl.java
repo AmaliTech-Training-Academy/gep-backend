673d9d95e95584a5a8a3a3254ed021d9904b253a
Roger Satsi <rogersatsi98@gmail.com>
2025-10-21 22:43:41 +0000

    feat: add event type service implementation

--- services/event-service/src/main/java/com/event_service/event_service/services/EventTypeServiceImpl.java
+++ services/event-service/src/main/java/com/event_service/event_service/services/EventTypeServiceImpl.java
@@ -1,4 +1,47 @@
 package com.event_service.event_service.services;
 
-public class EventTypeServiceImpl {
+import com.event_service.event_service.dto.EventTypeRequest;
+import com.event_service.event_service.dto.EventTypeResponse;
+import com.event_service.event_service.mappers.EventTypeMapper;
+import com.event_service.event_service.models.EventType;
+import com.event_service.event_service.repositories.EventTypeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.lang.module.ResolutionException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class EventTypeServiceImpl implements EventTypeService {
+
+    private final EventTypeRepository eventTypeRepository;
+    private final EventTypeMapper eventTypeMapper;
+
+    @Override
+    public void save(EventTypeRequest eventTypeRequest) {
+        EventType eventType = EventType.builder().name(eventTypeRequest.name()).build();
+        eventTypeRepository.save(eventType);
+    }
+
+    @Override
+    public EventType findById(Long id) {
+        return eventTypeRepository.findById(id)
+                .orElseThrow(()-> new ResolutionException("Event Type Not Found"));
+    }
+
+    @Override
+    public void update(Long id, EventTypeRequest eventTypeRequest) {
+        EventType eventType = findById(id);
+        eventType.setName(eventTypeRequest.name());
+        eventTypeRepository.save(eventType);
+    }
+
+    @Override
+    public List<EventTypeResponse> findAll() {
+        return eventTypeRepository.findAll().stream()
+                .map(eventTypeMapper::toEventTypeResponse)
+                .collect(Collectors.toList());
+    }
 }
