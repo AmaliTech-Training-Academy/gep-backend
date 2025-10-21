7a5959a67266e8f8288968c1dac0feec617991f6
Roger Satsi <rogersatsi98@gmail.com>
2025-10-21 22:40:26 +0000

    feat: add event option request dto

--- services/event-service/src/main/java/com/event_service/event_service/dto/EventOptionsRequest.java
+++ services/event-service/src/main/java/com/event_service/event_service/dto/EventOptionsRequest.java
@@ -1,4 +1,10 @@
 package com.event_service.event_service.dto;
 
-public record EventOptionsRequest() {
+import java.math.BigDecimal;
+
+public record EventOptionsRequest(
+        BigDecimal ticketPrice,
+        boolean requiresApproval,
+        Long capacity
+) {
 }
