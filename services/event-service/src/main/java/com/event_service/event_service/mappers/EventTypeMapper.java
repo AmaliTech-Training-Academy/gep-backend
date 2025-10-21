20e84649badffb09c8d2a0de391563982f01e08c
Roger Satsi <rogersatsi98@gmail.com>
2025-10-21 23:24:36 +0000

    feat: add event type mapper

--- services/event-service/src/main/java/com/event_service/event_service/mappers/EventTypeMapper.java
+++ services/event-service/src/main/java/com/event_service/event_service/mappers/EventTypeMapper.java
@@ -1,4 +1,10 @@
 package com.event_service.event_service.mappers;
 
+import com.event_service.event_service.dto.EventTypeResponse;
+import com.event_service.event_service.models.EventType;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface EventTypeMapper {
+    EventTypeResponse toEventTypeResponse(EventType eventType);
 }
