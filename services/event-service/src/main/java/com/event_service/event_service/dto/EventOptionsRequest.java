5f5464a945e3ac93535de60d176b2bdaac58c9b2
Roger Satsi <rogersatsi98@gmail.com>
2025-10-29 12:05:44 +0000

    feat: add event options request dto

--- services/event-service/src/main/java/com/event_service/event_service/dto/EventOptionsRequest.java
+++ services/event-service/src/main/java/com/event_service/event_service/dto/EventOptionsRequest.java
@@ -1,10 +1,17 @@
 package com.event_service.event_service.dto;
 
+import jakarta.validation.constraints.*;
+
 import java.math.BigDecimal;
 
 public record EventOptionsRequest(
+        @NotNull(message = "Ticket price is required")
+        @PositiveOrZero(message = "Ticket price must be non-negative number and greater than or equal to 0")
         BigDecimal ticketPrice,
+        @NotNull(message = "Event approval is required")
         boolean requiresApproval,
+        @NotNull(message = "Event capacity is required")
+        @Positive(message = "Event capacity must be a positive value")
         Long capacity
 ) {
 }
