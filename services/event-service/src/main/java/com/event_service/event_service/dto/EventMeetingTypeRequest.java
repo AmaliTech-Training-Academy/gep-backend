c7a9365314061eacedb71f01b0af581d06884dee
Roger Satsi <rogersatsi98@gmail.com>
2025-10-29 12:04:52 +0000

    feat: add event meeting type request dto

--- services/event-service/src/main/java/com/event_service/event_service/dto/EventMeetingTypeRequest.java
+++ services/event-service/src/main/java/com/event_service/event_service/dto/EventMeetingTypeRequest.java
@@ -1,6 +1,10 @@
 package com.event_service.event_service.dto;
 
+import com.event_service.event_service.models.enums.EventMeetingTypeEnum;
+import jakarta.validation.constraints.NotNull;
+
 public record EventMeetingTypeRequest(
-        String name
+        @NotNull(message = "Event meeting type is required")
+        EventMeetingTypeEnum name
 ) {
 }
