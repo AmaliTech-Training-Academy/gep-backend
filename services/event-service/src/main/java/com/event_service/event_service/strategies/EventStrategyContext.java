512ad0b8034a330bf449f04536b9496558eb7e7d
Roger Satsi <rogersatsi98@gmail.com>
2025-10-29 12:33:01 +0000

    feat: add event strategy context

---
+++ services/event-service/src/main/java/com/event_service/event_service/strategies/EventStrategyContext.java
@@ -0,0 +1,34 @@
+package com.event_service.event_service.strategies;
+
+
+import com.event_service.event_service.dto.EventRequest;
+import com.event_service.event_service.models.Event;
+import com.event_service.event_service.models.EventMeetingType;
+import com.event_service.event_service.models.EventType;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Component
+@Setter
+public class EventStrategyContext {
+
+    private EventStrategy eventStrategy;
+
+    public Event executeStrategy(
+            EventRequest eventRequest,
+            MultipartFile image,
+            List<MultipartFile> eventImages,
+            EventType eventType,
+            EventMeetingType eventMeetingType
+    ) {
+        return eventStrategy.createEvent(eventRequest,
+                image,
+                eventImages,
+                eventType,
+                eventMeetingType
+        );
+    }
+}
