c5bba0badad11cbaddb9530f2ade5677888d820c
Roger Satsi <rogersatsi98@gmail.com>
2025-10-29 12:41:43 +0000

    feat: add event service interface

--- services/event-service/src/main/java/com/event_service/event_service/services/EventService.java
+++ services/event-service/src/main/java/com/event_service/event_service/services/EventService.java
@@ -1,4 +1,11 @@
 package com.event_service.event_service.services;
 
+import com.event_service.event_service.dto.EventRequest;
+import com.event_service.event_service.dto.EventResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public interface EventService {
+    EventResponse createEvent(EventRequest eventRequest, MultipartFile image, List<MultipartFile> eventImages);
 }
