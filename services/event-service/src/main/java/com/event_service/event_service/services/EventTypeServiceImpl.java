268a297c520fd7d2eb95df35d83e2a0a02927a90
Roger Satsi <rogersatsi98@gmail.com>
2025-10-29 12:43:09 +0000

    feat: add method level security

--- services/event-service/src/main/java/com/event_service/event_service/services/EventTypeServiceImpl.java
+++ services/event-service/src/main/java/com/event_service/event_service/services/EventTypeServiceImpl.java
@@ -2,13 +2,14 @@ package com.event_service.event_service.services;
 
 import com.event_service.event_service.dto.EventTypeRequest;
 import com.event_service.event_service.dto.EventTypeResponse;
+import com.event_service.event_service.exceptions.ResourceNotFound;
 import com.event_service.event_service.mappers.EventTypeMapper;
 import com.event_service.event_service.models.EventType;
 import com.event_service.event_service.repositories.EventTypeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
-import java.lang.module.ResolutionException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,18 +21,21 @@ public class EventTypeServiceImpl implements EventTypeService {
     private final EventTypeMapper eventTypeMapper;
 
     @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void save(EventTypeRequest eventTypeRequest) {
         EventType eventType = EventType.builder().name(eventTypeRequest.name()).build();
         eventTypeRepository.save(eventType);
     }
 
     @Override
+    @PreAuthorize("isAuthenticated()")
     public EventType findById(Long id) {
         return eventTypeRepository.findById(id)
-                .orElseThrow(()-> new ResolutionException("Event Type Not Found"));
+                .orElseThrow(()-> new ResourceNotFound("Event Type Not Found"));
     }
 
     @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void update(Long id, EventTypeRequest eventTypeRequest) {
         EventType eventType = findById(id);
         eventType.setName(eventTypeRequest.name());
@@ -39,6 +43,7 @@ public class EventTypeServiceImpl implements EventTypeService {
     }
 
     @Override
+    @PreAuthorize("isAuthenticated()")
     public List<EventTypeResponse> findAll() {
         return eventTypeRepository.findAll().stream()
                 .map(eventTypeMapper::toEventTypeResponse)
