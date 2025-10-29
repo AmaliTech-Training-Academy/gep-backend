1e547caff7b2b9ad75b0152c966ef2bd40a22541
Roger Satsi <rogersatsi98@gmail.com>
2025-10-29 12:43:53 +0000

    feat: add time zone controller

---
+++ services/event-service/src/main/java/com/event_service/event_service/controllers/TimeZoneController.java
@@ -0,0 +1,24 @@
+package com.event_service.event_service.controllers;
+
+
+import com.event_service.event_service.dto.TimeZoneResponse;
+import com.event_service.event_service.utilities.TimeZoneUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/timezones")
+@RequiredArgsConstructor
+public class TimeZoneController {
+    private final TimeZoneUtils timeZoneUtils;
+
+    @GetMapping
+    public ResponseEntity<List<TimeZoneResponse>> getAllTimeZones() {
+        return ResponseEntity.ok(timeZoneUtils.getAllTimeZones());
+    }
+}
