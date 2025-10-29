be5f43ad1bc296f84f6758e845c7c77a542f6096
Roger Satsi <rogersatsi98@gmail.com>
2025-10-29 12:43:24 +0000

    feat: add method level security

--- services/event-service/src/main/java/com/event_service/event_service/services/EventMeetingTypeServiceImpl.java
+++ services/event-service/src/main/java/com/event_service/event_service/services/EventMeetingTypeServiceImpl.java
@@ -7,6 +7,7 @@ import com.event_service.event_service.mappers.EventMeetingMapper;
 import com.event_service.event_service.models.EventMeetingType;
 import com.event_service.event_service.repositories.EventMeetingTypeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,18 +21,21 @@ public class EventMeetingTypeServiceImpl implements EventMeetingTypeService {
     private final  EventMeetingMapper eventMeetingMapper;
 
     @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void createEventMeetingType(EventMeetingTypeRequest eventMeetingTypeRequest) {
         EventMeetingType eventMeetingType = EventMeetingType.builder().name(eventMeetingTypeRequest.name()).build();
         eventMeetingTypeRepository.save(eventMeetingType);
     }
 
     @Override
+    @PreAuthorize("isAuthenticated()")
     public EventMeetingType findEventMeetingTypeById(Long id) {
         return eventMeetingTypeRepository.findById(id)
                 .orElseThrow(()-> new ResourceNotFound("EventMeetingType not found"));
     }
 
     @Override
+    @PreAuthorize("isAuthenticated()")
     public List<EventMeetingTypeResponse> findAllEventMeetingTypes() {
         return eventMeetingTypeRepository.findAll()
                 .stream().map(eventMeetingMapper::toEventMeetingTypeResponse)
@@ -39,6 +43,7 @@ public class EventMeetingTypeServiceImpl implements EventMeetingTypeService {
     }
 
     @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void updateEventMeetingType(Long id, EventMeetingTypeRequest eventMeetingTypeRequest) {
         EventMeetingType eventMeetingType = findEventMeetingTypeById(id);
         eventMeetingType.setName(eventMeetingTypeRequest.name());
