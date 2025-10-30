e1c53673551986d1bb72b35af401377ec07c7290
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-10-30 09:44:07 +0000

    refactor: remove access and secret aws keys

--- services/auth-service/src/main/java/com/example/auth_service/config/AwsSqsConfig.java
+++ services/auth-service/src/main/java/com/example/auth_service/config/AwsSqsConfig.java
@@ -3,19 +3,11 @@ package com.example.auth_service.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Configuration
 public class AwsSqsConfig {
-
-    @Value("${spring.cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${spring.cloud.aws.credentials.secret-key}")
-    private String secretKey;
 
     @Value("${spring.cloud.aws.sqs.endpoint}")
     private String sqsEndpoint;
@@ -25,11 +17,6 @@ public class AwsSqsConfig {
         return SqsClient.builder()
                 .endpointOverride(java.net.URI.create(sqsEndpoint))
                 .region(Region.EU_WEST_1)
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKey,secretKey)
-                        )
-                )
                 .build();
     }
 
