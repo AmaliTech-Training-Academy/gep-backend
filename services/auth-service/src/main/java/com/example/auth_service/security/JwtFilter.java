1172ad7ce0a31eaa31fa47c554125bedffac238f
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-10-30 09:40:45 +0000

    refactor: validate token with user details

--- services/auth-service/src/main/java/com/example/auth_service/security/JwtFilter.java
+++ services/auth-service/src/main/java/com/example/auth_service/security/JwtFilter.java
@@ -30,18 +30,18 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = extractTokenFromCookie(request);
 
-        if (token != null && jwtUtil.validateToken(token)) {
+        if (token != null) {
                 String username = jwtUtil.extractUsername(token);
                 if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
                     UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-
-                    UsernamePasswordAuthenticationToken authToken =
-                            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                    if(jwtUtil.validateToken(token, userDetails)){
+                        UsernamePasswordAuthenticationToken authToken =
+                                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                        SecurityContextHolder.getContext().setAuthentication(authToken);
+                    }
 
                 }
-
         }
 
 
