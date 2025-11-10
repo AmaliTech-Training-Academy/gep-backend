1989c1a1e7bcc3ccfce5abcb7cff59f888fadccf
Roger Satsi <rogersatsi98@gmail.com>
2025-11-10 08:39:35 +0000

    feat: add list events method signature

--- services/event-service/src/main/java/com/event_service/event_service/services/EventService.java
+++ services/event-service/src/main/java/com/event_service/event_service/services/EventService.java
@@ -2,10 +2,25 @@ package com.event_service.event_service.services;
 
 import com.event_service.event_service.dto.EventRequest;
 import com.event_service.event_service.dto.EventResponse;
+import com.event_service.event_service.dto.ExploreEventResponse;
+import com.event_service.event_service.dto.PagedExploreEventResponse;
+import com.event_service.event_service.models.Event;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface EventService {
     EventResponse createEvent(EventRequest eventRequest, MultipartFile image, List<MultipartFile> eventImages);
+    PagedExploreEventResponse listEvents(
+            int pageNumber,
+            int pageSize,
+            String hasTitle,
+            String[] sortBy,
+            String location,
+            LocalDate date,
+            Boolean paid,
+            String priceFilter,
+            Boolean past
+    );
 }
