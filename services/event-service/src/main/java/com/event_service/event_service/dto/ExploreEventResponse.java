6d1d26807b8211c8d8bdfcb261d468bcbe87bbb6
Roger Satsi <rogersatsi98@gmail.com>
2025-11-10 08:33:22 +0000

    feat: add exploreevent response dto

---
+++ services/event-service/src/main/java/com/event_service/event_service/dto/ExploreEventResponse.java
@@ -0,0 +1,14 @@
+package com.event_service.event_service.dto;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public record ExploreEventResponse(
+        Long id,
+        String title,
+        String description,
+        Instant startTime,
+        String location,
+        String flyerUrl,
+        BigDecimal ticketPrice
+) {}
