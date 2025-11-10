ae21548aef8dd0d5dad1e99c790932ed44b861ba
Roger Satsi <rogersatsi98@gmail.com>
2025-11-10 08:41:45 +0000

    chore: remove dead code

--- services/event-service/src/main/java/com/event_service/event_service/services/EventService.java
+++ services/event-service/src/main/java/com/event_service/event_service/services/EventService.java
@@ -2,9 +2,7 @@ package com.event_service.event_service.services;
 
 import com.event_service.event_service.dto.EventRequest;
 import com.event_service.event_service.dto.EventResponse;
-import com.event_service.event_service.dto.ExploreEventResponse;
 import com.event_service.event_service.dto.PagedExploreEventResponse;
-import com.event_service.event_service.models.Event;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
