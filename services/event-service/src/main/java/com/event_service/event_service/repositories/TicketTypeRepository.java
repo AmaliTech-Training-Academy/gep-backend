e347a33e2a5a1d9c10d326fa0f4fb9adffebe4e1
XzibiT-Collins <huvisoncollins@gmail.com>
2025-11-11 00:01:09 +0000

    fix: fix bug where a single user with an email can buy many tickets by limiting the nnumber of tickets per head

--- services/event-service/src/main/java/com/event_service/event_service/repositories/TicketTypeRepository.java
+++ services/event-service/src/main/java/com/event_service/event_service/repositories/TicketTypeRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TicketTypeRepository extends JpaRepository<TicketType, Long> {
     List<TicketType> findAllByEvent(Event event);
+
+    Optional<TicketType> findByIdAndEvent(Long id, Event event);
 }
