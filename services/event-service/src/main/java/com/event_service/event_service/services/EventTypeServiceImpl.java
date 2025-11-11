5c594cfafa89395a429356324b6539339db98a7b
Roger Satsi <rogersatsi98@gmail.com>
2025-11-11 12:41:18 +0000

    fix: remove authentication for list events types method

--- services/event-service/src/main/java/com/event_service/event_service/services/EventTypeServiceImpl.java
+++ services/event-service/src/main/java/com/event_service/event_service/services/EventTypeServiceImpl.java
@@ -43,7 +43,6 @@ public class EventTypeServiceImpl implements EventTypeService {
     }
 
     @Override
-    @PreAuthorize("isAuthenticated()")
     public List<EventTypeResponse> findAll() {
         return eventTypeRepository.findAll().stream()
                 .map(eventTypeMapper::toEventTypeResponse)
