3d9a1dbf6bf7acd1a49fec02f3e467eb541e4002
Roger Satsi <rogersatsi98@gmail.com>
2025-11-17 14:02:47 +0000

    feat: add attendee registration count

--- services/event-service/src/main/java/com/event_service/event_service/mappers/EventMapper.java
+++ services/event-service/src/main/java/com/event_service/event_service/mappers/EventMapper.java
@@ -17,12 +17,12 @@ public interface EventMapper {
 
     @Mapping(target = "startTime", expression = "java(determineDisplayTime(event))")
     @Mapping(target = "ticketPrice", expression = "java(extractTicketPrice(event))")
-    @Mapping(target = "invitationCount", expression = "java(extractInvitationCount(event))")
+    @Mapping(target = "attendeeCount", expression = "java(extractInvitationCount(event))")
     ExploreEventResponse toExploreEventResponse(Event event);
 
     default Long extractInvitationCount(Event event) {
         if (event.getInvitations() == null) return 0L;
-        return (long) event.getInvitations().size();
+        return (long) event.getEventRegistrations().size();
     }
 
 
