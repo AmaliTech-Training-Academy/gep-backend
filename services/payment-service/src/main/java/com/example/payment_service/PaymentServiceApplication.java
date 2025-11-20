7f5332f9d5feb2e4e8a65f8834ab37e183e7ab1b
XzibiT-Collins <huvisoncollins@gmail.com>
2025-11-20 21:56:42 +0000

    fix: prevent RedisAuttoConfiguration in payment-service which causes Redis Bean issues when running the container

--- services/payment-service/src/main/java/com/example/payment_service/PaymentServiceApplication.java
+++ services/payment-service/src/main/java/com/example/payment_service/PaymentServiceApplication.java
@@ -2,10 +2,11 @@ package com.example.payment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
 @EnableJpaAuditing
 @ComponentScan(basePackages = {"com.example.payment_service", "com.example.common_libraries"})
 public class PaymentServiceApplication {
