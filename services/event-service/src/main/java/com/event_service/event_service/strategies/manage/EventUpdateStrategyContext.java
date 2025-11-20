9eb447164a17de1417d1af643cd2597e2cb4e3cf
Roger Satsi <rogersatsi98@gmail.com>
2025-11-20 12:16:13 +0000

    feat: add update event strategy context

--- services/event-service/src/main/java/com/event_service/event_service/strategies/manage/EventUpdateStrategyContext.java
+++ services/event-service/src/main/java/com/event_service/event_service/strategies/manage/EventUpdateStrategyContext.java
@@ -1,4 +1,41 @@
 package com.event_service.event_service.strategies.manage;
 
+
+import com.event_service.event_service.dto.EventRequest;
+import com.event_service.event_service.models.Event;
+import com.event_service.event_service.models.EventMeetingType;
+import com.event_service.event_service.models.EventType;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Component
+@Setter
 public class EventUpdateStrategyContext {
+
+    private UpdateEventStrategy updateEventStrategy;
+
+    public Event executeStrategy(
+            Long id,
+            EventRequest eventRequest,
+            MultipartFile image,
+            List<MultipartFile> newEventImages,
+            List<Long> imagesToRemove,
+            Event event,
+             EventType eventType,
+            EventMeetingType eventMeetingType
+    ) {
+        return updateEventStrategy.updateEvent(
+                id,
+                eventRequest,
+                image,
+                newEventImages,
+                imagesToRemove,
+                event,
+                eventType,
+                eventMeetingType
+        );
+    }
 }
