3ee5d73f55be86288a18f9b63f8ccf0a20b8cc81
Roger Satsi <rogersatsi98@gmail.com>
2025-11-20 14:03:24 +0000

    feat: add webhook data dto

---
+++ services/payment-service/src/main/java/com/example/payment_service/dto/WebhookData.java
@@ -0,0 +1,27 @@
+package com.example.payment_service.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record WebhookData(
+        long id,
+        String domain,
+        String status,
+        String reference,
+        int amount,
+        String message,
+        String gateway_response,
+        String paid_at,
+        String created_at,
+        String channel,
+        String currency,
+        String ip_address,
+        Map<String, Object> metadata,
+        Integer fees,
+        Object fees_breakdown,
+        WebhookAuthorization authorization,
+        WebhookCustomer customer,
+        int requested_amount
+) {}
