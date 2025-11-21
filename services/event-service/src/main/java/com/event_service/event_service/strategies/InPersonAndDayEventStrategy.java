1a775192937ae4e85aaa1bfc69cd5c204d7689cd
Roger Satsi <rogersatsi98@gmail.com>
2025-11-21 10:59:57 +0000

    feat: add event section implementation
    # Conflicts:
    #	services/event-service/src/main/java/com/event_service/event_service/strategies/InPersonAndDayEventStrategy.java

--- services/event-service/src/main/java/com/event_service/event_service/strategies/InPersonAndDayEventStrategy.java
+++ services/event-service/src/main/java/com/event_service/event_service/strategies/InPersonAndDayEventStrategy.java
@@ -1,6 +1,7 @@
 package com.event_service.event_service.strategies;
 
 import com.event_service.event_service.dto.EventRequest;
+import com.event_service.event_service.dto.EventSectionRequest;
 import com.event_service.event_service.models.*;
 import com.event_service.event_service.repositories.EventRepository;
 import com.event_service.event_service.utils.TimeZoneUtils;
@@ -32,7 +33,23 @@ public class InPersonAndDayEventStrategy implements EventStrategy {
                              MultipartFile image,
                              List<MultipartFile> eventImages,
                              EventType eventType,
-                             EventMeetingType eventMeetingType) {
+                             EventMeetingType eventMeetingType,
+                             List<MultipartFile> sectionImages
+                             ) {
+
+        List<EventSectionRequest> sections =
+                eventRequest.eventSectionRequest() != null
+                        ? eventRequest.eventSectionRequest()
+                        : List.of();
+
+        List<MultipartFile> sectionImagesList =
+                sectionImages != null
+                        ? sectionImages
+                        : List.of();
+
+        if (sections.size() != sectionImagesList.size()) {
+            throw new IllegalArgumentException("Section count and image count must match.");
+        }
 
         EventOptions eventOptions = EventOptions.builder()
                 .ticketPrice(eventRequest.eventOptionsRequest().ticketPrice())
@@ -66,6 +83,8 @@ public class InPersonAndDayEventStrategy implements EventStrategy {
                 .userId(authenticatedUser.id())
                 .build();
 
+        createVenueSections(sections, sectionImagesList, event);
+
         attachEventImages(eventImages, event);
 
         createFreeTicket(eventRequest, event);
@@ -74,6 +93,28 @@ public class InPersonAndDayEventStrategy implements EventStrategy {
 
 
         return eventRepository.save(event);
+    }
+
+
+    private void createVenueSections(List<EventSectionRequest> sections, List<MultipartFile> sectionImagesList, Event event) {
+        for (int i = 0; i < sections.size(); i++) {
+            EventSectionRequest req = sections.get(i);
+            MultipartFile img = sectionImagesList.get(i);
+
+            String imageUrl = s3Service.uploadImage(img);
+
+            EventSection section = EventSection.builder()
+                    .name(req.name())
+                    .capacity(req.capacity().intValue())
+                    .price(req.price())
+                    .description(req.description())
+                    .color(req.color())
+                    .imageUrl(imageUrl)
+                    .event(event)
+                    .build();
+
+            event.addSection(section);
+        }
     }
 
     private static void createFreeTicket(EventRequest eventRequest, Event savedEvent) {
