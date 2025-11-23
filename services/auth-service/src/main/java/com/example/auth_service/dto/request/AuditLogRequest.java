75379405ced96690d3e124241d7334c74e3bcf42
Roger Satsi <rogersatsi98@gmail.com>
2025-11-23 21:35:36 +0000

    feat: add audit log data status enum

--- services/auth-service/src/main/java/com/example/auth_service/dto/request/AuditLogRequest.java
+++ services/auth-service/src/main/java/com/example/auth_service/dto/request/AuditLogRequest.java
@@ -1,11 +1,14 @@
 package com.example.auth_service.dto.request;
 
+import com.example.auth_service.enums.AuditStatus;
+
 import java.time.Instant;
 
 public record AuditLogRequest(
         String email,
         String ipAddress,
-        Instant timestamp
+        Instant timestamp,
+        AuditStatus auditStatus
 ) {
 }
 
