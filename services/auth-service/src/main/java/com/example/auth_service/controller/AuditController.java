dba13e2a93651ef4ad377992d778ab5a408f9c60
Roger Satsi <rogersatsi98@gmail.com>
2025-11-23 21:41:24 +0000

    feat: add new controller for audit log retrieval

--- services/auth-service/src/main/java/com/example/auth_service/controller/AuditController.java
+++ services/auth-service/src/main/java/com/example/auth_service/controller/AuditController.java
@@ -1,8 +1,11 @@
 package com.example.auth_service.controller;
 
+import com.example.auth_service.dto.response.EnrichedAuditResponse;
 import com.example.auth_service.dto.response.PagedAuditResponse;
 import com.example.auth_service.service.AuditService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +21,12 @@ public class AuditController {
     private final AuditService auditService;
 
     @GetMapping
-    public ResponseEntity<PagedAuditResponse> getAuditLogs(
-            @RequestParam(value = "sortBy", defaultValue = "createdAt",required = false) String[] sortBy,
-            @RequestParam(value = "pageNumber",defaultValue = "0", required = false) int pageNumber,
-            @RequestParam(value = "pageSize", defaultValue = "5", required = false) int pageSize
+    public PagedAuditResponse<EnrichedAuditResponse> getAuditLogs(
+            @RequestParam(defaultValue = "0") int pageNumber,
+            @RequestParam(defaultValue = "10") int pageSize
     ) {
-        return ResponseEntity.ok(auditService.findAll(pageNumber, pageSize,sortBy));
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        return auditService.getEnrichedAuditLogs(pageable);
     }
 }
 
