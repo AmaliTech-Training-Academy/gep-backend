701d8a71fa612840c7b7b5cdc124465e62fd4de9
Roger Satsi <rogersatsi98@gmail.com>
2025-11-23 21:38:25 +0000

    feat: add pagd enriched logs retireval method

--- services/auth-service/src/main/java/com/example/auth_service/service/AuditService.java
+++ services/auth-service/src/main/java/com/example/auth_service/service/AuditService.java
@@ -1,9 +1,12 @@
 package com.example.auth_service.service;
 
 import com.example.auth_service.dto.request.AuditLogRequest;
+import com.example.auth_service.dto.response.EnrichedAuditResponse;
 import com.example.auth_service.dto.response.PagedAuditResponse;
+import org.springframework.data.domain.Pageable;
 
 public interface AuditService {
     void save(AuditLogRequest auditLogRequest);
-    PagedAuditResponse findAll(int pageNumber, int pageSize, String[] sortBy);
+    PagedAuditResponse<EnrichedAuditResponse> getEnrichedAuditLogs(Pageable pageable);
+
 }
